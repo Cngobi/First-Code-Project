36ba3308a095008cb34e6152ead28c0a7afd7dfe
Cngobi <83876486+Cngobi@users.noreply.github.com>
2021-05-09 20:23:59 +0100

    Add files via upload

---
+++ Exam 25th Jan/Loan Payoff Calculator.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Loan Payoff Calculator.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Loan Payoff Calculator</title>
+    <style type="text/css">
+        .newStyle1 {
+            text-align: right;<a href="Loan%20Payoff%20Calculator.aspx">Loan Payoff Calculator.aspx</a>
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Loan, £<br />
+            <asp:TextBox ID="TextBox1" runat="server" CssClass="newStyle1"></asp:TextBox>
+            <br />
+            <br />
+            Monthly Interest Rate<br />
+            <asp:TextBox ID="TextBox2" runat="server" CssClass="newStyle1"></asp:TextBox>
+            %<br />
+            <br />
+            Monthly Payment, £<br />
+            <asp:TextBox ID="TextBox3" runat="server" CssClass="newStyle1"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" CssClass="newStyle1" OnClick="Button1_Click" Text="Evaluate Debt" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Exam 25th Jan/Loan Payoff Calculator.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        double loan = Convert.ToDouble(TextBox1.Text);
+        double interestRate = Convert.ToDouble(TextBox2.Text);
+        double MonthlyPayment = Convert.ToDouble(TextBox3.Text); // Converts the value to a double-precision floating-point number
+        for (int year = 1; loan >= 0; year++) // Setting the outer loop for the year count 
+        {  
+            Label1.Text += "<strong>Year: </strong>" + "<strong>" + Convert.ToString(year) + "</strong>" + "<br />"; // This line of code will generate the year count/value
+            for (int month = 1; loan >= 0 & month < 13; month++) // Setting the for loop for months up to the 12th month
+            {
+                double LeftOver = loan * interestRate / 100 + loan;
+                loan += loan * interestRate / 100 - MonthlyPayment; // Here are the equations to calculate the loan payoff
+                loan = Math.Round(loan, 2);   // Rounding the value to 2 decimal places
+
+                    if (loan <= 0) // This loop generates when the amount reaches £0.00
+                    {
+                        Label1.Text += "Month " + month.ToString() + " => £ " + "0.00" + "<br />"; // This line of code will be displayed when the amount reaches £0.00 
+                        Label1.Text += "Last Payment: £" + LeftOver.ToString("F2"); // This line of code will display the final payment amount
+
+                        break; // This causes the loop to be terminated and will continue onto the next label
+                    }
+
+                Label1.Text += "Month " + month.ToString() + " => £ " + loan.ToString("F2") + "<br />"; // If the final value is not <=0, this label will be presented and show the value for each month         
+            }
+        }
+    }
+}
